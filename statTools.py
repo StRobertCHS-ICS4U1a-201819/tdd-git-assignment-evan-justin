c8b736f76fdd9e28fed42ef5d066d4b48cdf9b8b
ChainZ0121 <38053626+ChainZ0121@users.noreply.github.com>
2018-11-02 23:05:09 -0400

    fuction mean() is finished and tested, function median is waiting to be finished

--- statTools.py
+++ statTools.py
@@ -1,1 +1,17 @@
+def mean(myList):
+    if myList == []:
+        return "The list is empty"
+    else:
+        total = 0
+        for i in myList:
+            total += i
+        avg = total/(len(myList))
+        return avg
 
+
+def median(myList):
+    if myList == []:
+        return "The list is empty"
+    else:
+        if len(myList) % 2 == 1:
+            medIndex = len(myList)//2 + 1
--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,25 @@
+from pytest import *
+from statTools import *
 
+# testing function mean
+def test_mean_base1():
+    assert(mean([1,2,3]) == 2)
+
+def test_mean_base2():
+    assert(mean([3,3,3,3]) == 3)
+
+def test_mean_empty():
+    assert(mean([]) == "The list is empty")
+
+#testing function median()
+def test_median_base1():
+    assert(median([1,2,3]) == 2)
+
+def test_median_base2():
+    assert(median([1,3,2]) == 2)
+
+def test_median_base3():
+    assert(median([3,3,3]) == 3)
+
+def test_median_empty():
+    assert(median([]) == "The list is empty")
