f1b3e18ef242a4f11943304c8d722b10bd0b0a0c
ChainZ0121 <38053626+ChainZ0121@users.noreply.github.com>
2018-11-11 14:18:56 -0500

    merge from master

--- statTools.py
+++ statTools.py
@@ -1,17 +1,94 @@
-def mean(myList):
-    if myList == []:
-        return "The list is empty"
-    else:
-        total = 0
-        for i in myList:
-            total += i
-        avg = total/(len(myList))
-        return avg
+#-------------------------------------------------------------------------------
+# Name:	statTool.py
+# Purpose:
+# to create a single python module called statTools.py that contains functions
+# for Measures of Central Tendency and Measures of Spread.
+#
+# Author:	Evan Bai / Justin Guo
+#
+# Created:	10/11/2018
+#-------------------------------------------------------------------------------
 
 
-def median(myList):
-    if myList == []:
+# define a function mean()get the avg of a list of number
+def mean(int_List):
+   if int_List == []:
+       return "The list is empty"
+   else:
+       total = 0
+       for i in int_List:
+           total += i
+       avg = total/(len(int_List))
+       return avg
+
+# def a function median() to get the the middle item when the data are arranged from lowest to highest
+def median(int_List):
+    int_List = sorted(int_List)
+    if int_List == []:
         return "The list is empty"
     else:
-        if len(myList) % 2 == 1:
-            medIndex = len(myList)//2 + 1
+        int_List = sorted(int_List)
+        if len(int_List) % 2 == 1:
+           medIndex = len(int_List) // 2 + 1
+           return int_List[medIndex - 1]
+        else:
+           medIndex1 = len(int_List) / 2
+           medIndex2 = len(int_List) / 2 + 1
+           return (int_List[medIndex1 - 1] + int_List[medIndex2 - 1]) / 2
+
+# define a function mode() to get the observation that occurs most frequently in the list
+def mode(int_List):
+   int_List = sorted(int_List)
+   count = 0
+   num1 = 0
+   num2 = 0
+   if int_List == []:
+       return "The list is empty"
+   for i in int_List:
+       volCount = int_List.count(i)
+       if volCount > count:
+           count = volCount
+           num1 = i
+       elif volCount == count:
+           num2 = i
+   if num1 == num2:
+       return num1
+   else:
+       return "no unique mode"
+
+# define a function Range() to get the difference between the largest and smallest result in the list
+def Range(int_List):
+   if int_List == []:
+       return "The list is empty"
+   else:
+       return max(int_List) - min(int_List)
+
+# define a function lowerQuartile() to get the lower quartile of the list
+def lowerQuartile(int_List):
+    int_List = sorted(int_List)
+    if int_List == []:
+        return "The list is empty"
+    else:
+        int_List = sorted(int_List)
+        if (len(int_List) + 1) % 4 != 0:
+            lowerQuartileIndex1 = (len(int_List) + 1) // 4
+            lowerQuartileIndex2 = (len(int_List) + 1) // 4 + 1
+            return (int_List[lowerQuartileIndex1 - 1] + int_List[lowerQuartileIndex2 - 1]) / 2
+        else:
+            lowerQuartileIndex = (len(int_List) + 1) // 4
+            return int_List[lowerQuartileIndex - 1]
+
+# define a function lowerQuartile() to get the upper quartile of the list
+def upperQuartile(int_List):
+    int_List = sorted(int_List)
+    if int_List == []:
+        return "The list is empty"
+    else:
+        int_List = sorted(int_List)
+        if (len(int_List) + 1) % 4 != 0:
+            upperQuartileIndex1 = (len(int_List) + 1) * 3 // 4
+            upperQuartileIndex2 = (len(int_List) + 1) * 3 // 4 + 1
+            return (int_List[upperQuartileIndex1 - 1] + int_List[upperQuartileIndex2 - 1]) / 2
+        else:
+            upperQuartileIndex = (len(int_List) + 1) * 3 // 4
+            return int_List[upperQuartileIndex - 1]
--- test_statTools.py
+++ test_statTools.py
@@ -1,6 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:	test_statTool.py
+# Purpose:
+# to test every single function in python module statTools.py
+#
+# Author:	Evan Bai / Justin Guo
+#
+# Created:	10/11/2018
+#-------------------------------------------------------------------------------
+
+
 from pytest import *
 from statTools import *
 
+# testing of central Tendency
 # testing function mean
 def test_mean_base1():
     assert(mean([1,2,3]) == 2)
@@ -8,10 +20,13 @@ def test_mean_base1():
 def test_mean_base2():
     assert(mean([3,3,3,3]) == 3)
 
+def test_mean_base3():
+    assert(mean([1,2,3,4]) == 2.5)
+
 def test_mean_empty():
     assert(mean([]) == "The list is empty")
 
-#testing function median()
+# testing function median()
 def test_median_base1():
     assert(median([1,2,3]) == 2)
 
@@ -23,3 +38,45 @@ def test_median_base3():
 
 def test_median_empty():
     assert(median([]) == "The list is empty")
+
+# testing function mode()
+def test_mode_base1():
+    assert(mode([1,1,1,2,2,3]) == 1)
+
+def test_mode_base2():
+    assert (mode([1,1,1,2,2,2,3]) == "no unique mode")
+
+def test_mode_base3():
+    assert (mode([1,2,3,1,2,1]) == 1)
+
+def test_mode_empty():
+    assert (mode([]) == "The list is empty")
+
+
+# testing the Measures of Spread
+# testing the function range()
+def test_range_base1():
+    assert(Range([1,2,3,4,5]) == 4)
+
+def test_range_base2():
+    assert(Range([1,3,5,2,4,6]) == 5)
+
+def test_range_base3():
+    assert(Range([1,1,1,1,1]) == 0)
+
+def test_range_empty():
+    assert(Range([]) == "The list is empty")
+
+# testing the function upperQuartile
+def test_upperQuartile_base1():
+    assert(upperQuartile([1,2,3,4]) == 3.5)
+
+def test_upperQuartile_base2():
+    assert(upperQuartile([18,20,23,20,23,27,24,23,29]) == 25.5)
+
+def test_upperQuartile_base3():
+    assert(upperQuartile([1,2,3]) == 3)
+
+def test_upperQuartile_empty():
+    assert(upperQuartile([]) == "The list is empty")
+
